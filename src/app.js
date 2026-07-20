fa6cfd18445f10edca3340fffc31a1d4fe2ff713
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-07-20 20:26:47 +0500

    till creating notes app use rest api with backend

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 // app.js server ko create karta hai
 import express from "express";
 const app = express();
-
-
 app.use(express.json())
 
 const notes = []
 
-console.log(notes,"this is array")
-
-
+// POST /notes
 app.post("/notes", (req, res) => {
     notes.push(req.body)
 
@@ -18,12 +14,37 @@ app.post("/notes", (req, res) => {
     })
 })
 
-app.get("/notes", (req,res)=>{
+
+// GET /notes
+app.get("/notes", (req, res) => {
     res.status(200).json({
         message: "notes fatch successfully",
-        notes : notes
+        notes: notes
     })
 })
+
+// DELETE /note/:index
+app.delete("/notes/:index", (req, res) => {
+    const index = req.params.index
+
+    delete notes[ index ]
+
+    res.status(200).json({
+        message: "note deleted successfully"
+    })
+})
+
+app.patch("/notes/:index", (req, res)=>{
+    const index = req.params.index
+    const discription = req.body.description
+
+    notes [ index ].discription = discription
+
+    res.status(200).json({
+        message: "notes updated successfully"
+    })  
+})
+
 
 
 export default app
